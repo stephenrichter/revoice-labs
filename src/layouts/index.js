9f49beb9800f9eeec50cfe86c5323dbbe2460bfc
Stephen Richter <stephenrichter15@gmail.com>
2018-04-05 15:47:22 -0700

    Added GSuite verification.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,6 +9,7 @@ const TemplateWrapper = ({ children }) => (
   <div className="has-navbar-fixed-top">
     <Helmet title="Revoice Labs | Welcome to the Voice Revolution">
       <link rel="shortcut icon" type="image/png" href='img/revoice_favicon.png' />
+      <meta name="google-site-verification" content="19CJ-fmYyw3ccK84WSXNijid8U6mT8NykE3Oi8JUuTI" />
     </Helmet>
     <Navbar />
     <div>{children()}</div>
